23d46c4914821eb773ac63817b5760a30e4a1467
benben-prog <benben@benb3n.com>
2026-08-28 20:26:06 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,16 +33,18 @@ const swaggerSpec = require("./docs/swagger");
 const app = express();
 
 // ============================================
-// OPTIONS HANDLER - MUST BE VERY FIRST
+// CORS HEADERS - MUST BE VERY FIRST
 // ============================================
 
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-client-key, x-super-admin-key");
+
   if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, x-client-key, x-super-admin-key");
     return res.status(200).end();
   }
+
   next();
 });
 
